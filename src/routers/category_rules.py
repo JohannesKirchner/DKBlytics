5bfa8a09ec5d316904ec9cfaa7cb5bfc4ebeb71b
JohannesKirchner <johannes.kirchner@uni-muenster.de>
2025-09-01 21:32:02 +0200

    clean outdated category rules

--- src/routers/category_rules.py
+++ src/routers/category_rules.py
@@ -1,15 +1,12 @@
 from typing import List, Optional
-from fastapi import APIRouter, HTTPException, Query, Depends, status
+from fastapi import APIRouter, Query, Depends, status
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..schemas import CategoryRule, CategoryRuleCreate
 from ..services.category_rules import (
     create_category_rule_db,
-    get_category_rule_db,
     get_category_rule_by_id_db,
     get_all_category_rules_db,
-    update_category_rule_by_text_and_entity_db,
-    update_category_rule_by_entity_db,
 )
 
 router = APIRouter(
@@ -54,36 +51,3 @@ def get_category_rule_by_id(
     """
     category_rule = get_category_rule_by_id_db(db, category_id)
     return category_rule
-
-
-@router.patch("/by-entity/{entity}")
-def update_category_rules_by_entity(
-    entity: str, category_id: int, db: Session = Depends(get_db)
-):
-    """
-    Updates the category for all rules matching a given entity.
-    """
-    updated_count = update_category_rule_by_entity_db(db, entity, category_id)
-    if updated_count == 0:
-        raise HTTPException(
-            status_code=404, detail=f"No category rules found for entity '{entity}'"
-        )
-    return {
-        "message": f"Successfully updated {updated_count} categories for entity '{entity}'."
-    }
-
-
-@router.patch("/{text}/{entity}", response_model=CategoryRule)
-def update_category_rule(
-    text: str, entity: str, category_id: int, db: Session = Depends(get_db)
-):
-    """
-    Updates the category name for a specific existing category rule.
-    """
-    if not update_category_rule_by_text_and_entity_db(db, text, entity, category_id):
-        raise HTTPException(status_code=404, detail="Category rule not found")
-
-    updated_category_data = get_category_rule_db(db, text, entity)
-    if updated_category_data:
-        return CategoryRule(**dict(updated_category_data))
-    raise HTTPException(status_code=404, detail="Category not found after update.")
